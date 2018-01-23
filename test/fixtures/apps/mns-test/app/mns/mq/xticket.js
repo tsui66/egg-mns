f5b1366ac1909c40e68b335ac61f2798d22964e7
Quinton.Xu <quinton.xu@gmail.com>
2018-01-23 15:32:16 +0800

    bump version to 0.0.7

--- test/fixtures/apps/mns-test/app/mns/mq/xticket.js
+++ test/fixtures/apps/mns-test/app/mns/mq/xticket.js
@@ -1,10 +1,12 @@
 'use strict';
 
-module.exports = (err, message) => {
-  console.log(message);
-  if(err && err.message === "NetworkBroken"){
-      // Best to restart the process when this occurs
-      throw err;
+module.exports = app => {
+  return function (err, message) {
+    console.log(message);
+    if(err && err.message === "NetworkBroken"){
+        // Best to restart the process when this occurs
+        throw err;
+    }
+    return true; // this will cause message to be deleted automatically
   }
-  return true; // this will cause message to be deleted automatically
 };
